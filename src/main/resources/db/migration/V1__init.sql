bc294dd53916a6479db24ccc997543c5f0aca6f8
MaksGavriliuk <maks.revival27@gmail.com>
2024-02-27 01:46:38 +0300

    refactor: change docker port

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,14 +1,14 @@
 CREATE TABLE faculties
 (
-    id      BIGSERIAL INT PRIMARY KEY,
-    name    VARCHAR(255) NOT NULL ,
-    dean_id INT NOT NULL
+    id      BIGSERIAL PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL,
+    dean_id BIGINT          NOT NULL
 );
 
 CREATE TABLE departments
 (
-    id         BIGSERIAL INT PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL ,
-    faculty_id INT NOT NULL ,
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    faculty_id BIGINT       NOT NULL,
     FOREIGN KEY (faculty_id) REFERENCES faculties (id)
 );
