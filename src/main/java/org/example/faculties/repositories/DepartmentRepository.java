b4d69b8d2d05d401114f4fee0b873d30c448d646
MaksGavriliuk <maks.revival27@gmail.com>
2024-02-29 00:29:45 +0300

    create: jpa repositories

---
+++ src/main/java/org/example/faculties/repositories/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package org.example.faculties.repositories;
+
+
+import org.example.faculties.entities.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
---
+++ src/main/java/org/example/faculties/repositories/FacultyRepository.java
@@ -0,0 +1,10 @@
+package org.example.faculties.repositories;
+
+
+import org.example.faculties.entities.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+}
